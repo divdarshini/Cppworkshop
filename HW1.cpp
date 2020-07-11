c34c00e578ee55dd5f94ca90dea37acdd38243b4
divdarshini <68019444+divdarshini@users.noreply.github.com>
2020-07-11 15:33:33 +0530

    Operators and Data Types
    1st assignment.

---
+++ HW1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+ /*	int i;
+ 	float j;
+ 	char c;
+ 	double d;
+ */	
+ 	cout<<"Size of char:"<<sizeof(char)<<" bytes\n";
+ 	cout<<"Size of int:"<<sizeof(int)<<" bytes\n";
+ 	cout<<"Size of float:"<<sizeof(float)<<" bytes\n";
+ 	cout<<"Size of double:"<<sizeof(double)<<" bytes\n";
+ 	
+    return 0;
+ }
---
+++ HW2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float i, j;
+cout<<"Enter two numbers:\n";
+cin>>i>>j;
+cout<<"Product= "<<i*j;
+return 0;
+
+}
+
