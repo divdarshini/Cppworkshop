e31b54c3334f6116eb5e12134ebc2db52e6a0702
divdarshini <68019444+divdarshini@users.noreply.github.com>
2020-07-26 21:12:05 +0530

    Classes and OOPS
    An assignment to convert hours, minutes and seconds entered by user into seconds using classes and inheritance in C++.

---
+++ CLASS.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+class enterhr
+{
+protected:
+int h=0;
+public:
+ void gethour()
+ {
+ 	cout<<"Enter hours value:"<<endl;
+ 	cin>>h;
+ 	
+ }	
+
+	
+};
+	
+
+
+class entermin
+{
+protected:
+int m=0;
+public:
+ void getmin()
+ {
+ 	cout<<"Enter minutes value:"<<endl;
+ 	cin>>m;
+}
+	
+};
+
+class entersec:public entermin
+{
+	protected:
+	int s=0;
+	public:
+	void getsec()
+	{
+	cout<<"Enter seconds value:"<<endl;
+	cin>>s;
+    }
+	
+	
+	
+};
+
+class converter:public entersec, public enterhr
+{
+	public:
+		int res=0;
+		void convert()
+		{   cout<<"The time is= "<<h<<":"<<m<<":"<<s<<endl;
+			res=(h*3600)+(m*60)+s;
+			cout<<"Time in total seconds: "<<res<<endl;
+		}
+	
+	
+	
+};
+
+int main()
+{
+	converter con;
+	con.gethour();
+	con.getmin();
+	con.getsec();
+	con.convert();
+	return 0;}
